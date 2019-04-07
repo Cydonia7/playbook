c6a4cc8f6db2c82455916fb477822134c3988542
Thomas Royer <cydonia572@gmail.com>
2019-04-08 00:58:58 +0200

    Basic provision script

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+export ANSIBLE_VERSION=2.7.10
+export PATH="$HOME/.local/bin:$PATH"
+
+pip install --user virtualenv
+virtualenv venv
+source venv/bin/activate
+
+pip install ansible==$ANSIBLE_VERSION
+ansible-playbook --ask-become-pass -i inventory.ini playbook.yml
