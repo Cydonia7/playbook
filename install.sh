e5107c204a18236a030a3beaec64cecebeca67a5
Thomas Royer <Cydonia7@users.noreply.github.com>
2019-04-09 14:24:44 +0200

    Shellcheck advice

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 
 export ANSIBLE_VERSION=2.7.10
 export PATH="$HOME/.local/bin:$PATH"
 
 pip install --user virtualenv
 virtualenv venv
-source venv/bin/activate
+. venv/bin/activate
 
 pip install ansible==$ANSIBLE_VERSION
 ansible-playbook --ask-become-pass -i inventory.ini playbook.yml
