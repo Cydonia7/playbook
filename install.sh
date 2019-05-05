3b6ef3890025423e16be4e49b69c8883bb0ada85
Thomas Royer <cydonia572@gmail.com>
2019-05-05 11:46:44 +0200

    Good scrollback on alacritty

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ virtualenv venv
 . venv/bin/activate
 
 wget -N https://networkgenomics.com/try/mitogen-0.2.6.tar.gz
-tar xvf mitogen-0.2.6.tar.gz
+tar xf mitogen-0.2.6.tar.gz
 
 pip install ansible==$ANSIBLE_VERSION
 ansible-playbook --ask-become-pass -i inventory.ini playbook.yml
